58d0a2eeef5a12b6e2ad5d5e595d3d6774b9e14f
Ben Clark <hello@clarkben.com>
2021-08-31 10:16:23 +0100

    Adds logic to scrape each data field from page

--- scrape.js
+++ scrape.js
@@ -7,11 +7,43 @@ function scrape() {
 
     const videoDescription = document.querySelector('#description') ? document.querySelector('#description').textContent : null ;
     if (videoDescription) {
-        const playerStart = videoDescription.indexOf('Player:');
-        const playerEnd = videoDescription.slice(playerStart).indexOf(' |');
-        const player = videoDescription.substring(playerStart, playerEnd);
+        // Player
+        const playerStart = videoDescription.split("Player:")[1];
+        const player = playerStart?.split("|")[0].trim();
         console.log(player);
+        // Class
+        const charClassStart = videoDescription.split("Class:")[1];
+        const charClass = charClassStart?.split("|")[0].trim();
+        console.log(charClass);
+        // Spec
+        const specStart = videoDescription.split("Spec:")[1];
+        const spec = specStart?.split("|")[0].trim();
+        console.log(spec);
+        // Level
+        const levelStart = videoDescription.split("Level:")[1];
+        const level = levelStart?.split("|")[0].trim();
+        console.log(level);
+        // URL
+        const url = window.location.href;
+        console.log(url);
+        // Timestamp
+        const currentTime = document.querySelector(".ytp-time-current").textContent;
+        const timestampSplitArray = currentTime.split(":");
+        const count = (currentTime.match(/:/g) || []).length;
+        if (count === 1) {
+            // mins and secs
+            const minsConvertedToSeconds = Number.parseInt(timestampSplitArray[0], 10) * 60;
+            const secs = Number.parseInt(timestampSplitArray[1], 10) + minsConvertedToSeconds;
+        }
+        if (count === 2) {
+            // hours, mins and secs
+            const hoursConvertedToSeconds = Number.parseInt(timestampSplitArray[0],10) * 60 * 60;
+            const minsConvertedToSeconds = Number.parseInt(timestampSplitArray[0],10) * 60;
+            const secs = Number.parseInt(timestampSplitArray[1],10) + minsConvertedToSeconds + hoursConvertedToSeconds;
+        }
+        console.log(secs);
     }
+
 }
 
 const scrapeButton = document.querySelector('[data-lp-scrape]');
